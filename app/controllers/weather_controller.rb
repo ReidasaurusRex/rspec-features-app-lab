11bbdb1c5a404554b9c35b6bbac9e5e9d26fdd85
ReidasaurusRex <reidhlewis91@gmail.com>
2015-03-05 09:52:00 -0800

    Added above or below 50k zipcode functionality

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,6 +4,14 @@ class WeatherController < ApplicationController
   end
 
   def search 
-
+    
+    if (params[:zipcode].to_i > 50000)
+      @sun = "Sun's gonna be shining!"
+      @umbrella = "No umbrella needed."  
+    elsif (params[:zipcode].to_i < 50001)
+      @sun = "It's Gon Rain!"
+      @umbrella = "Bring an umbrella."
+    end
+  
   end
 end
